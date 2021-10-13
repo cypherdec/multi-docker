30178f540c04f96b3a9c2212b71456c6a8b976a2
root <root@LAPTOP-KSMMO3AK.localdomain>
2021-10-13 17:20:41 +0530

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ function App() {
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
+          <p>hey i just edited this</p>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
